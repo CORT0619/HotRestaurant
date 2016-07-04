65b3da0c7001ed18e3ceddee5632c9de8ddb5fbd
Crystal <cortney0215@yahoo.com>
2016-07-03 23:22:24 -0400

    updated reserve and tables templates and html-routes.js

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -19,13 +19,15 @@ module.exports = function(app){
 	// In each of the below cases the user is shown an HTML page of content
 	// ---------------------------------------------------------------------------
 
-	/*app.get('/tables', function(req, res){
-		res.sendFile(path.join(__dirname + '/../public/tables.html'));
+	app.get('/tables', function(req, res){
+		//res.sendFile(path.join(__dirname + '/../public/tables.html'));
+		res.render('tables');
 	});
 
 	app.get('/reserve', function(req, res){
-		res.sendFile(path.join(__dirname + '/../public/reserve.html'));
-	});*/
+		//res.sendFile(path.join(__dirname + '/../public/reserve.html'));
+		res.render('reserve');
+	});
 
 	// If no matching route is found default to home
 	app.use(function(req, res){
